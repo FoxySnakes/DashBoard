398d6ef522f38a4957f8bc7d4f18087dd115e544
FoxySnakes <matthox45@gmail.com>
2023-05-27 12:53:51 +0200

    try fix nav bottom on mobile

--- src/app/modules/main-layout/navigation-mobile/navigation-mobile.component.ts
+++ src/app/modules/main-layout/navigation-mobile/navigation-mobile.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navigation-mobile',
   templateUrl: './navigation-mobile.component.html',
   styleUrls: ['./navigation-mobile.component.scss', '../../../../assets/icon.scss']
 })
-export class NavigationMobileComponent {
+export class NavigationMobileComponent implements OnInit {
 
+  test = ""
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.adjustBottom();
+    window.addEventListener('resize', this.adjustBottom);
+  }
+
+  adjustBottom() {
+    const nav = document.getElementById('nav-bottom');
+    let windowHeight = window.innerHeight;
+    let documentHeight = document.documentElement.clientHeight;
+    if (nav && windowHeight != documentHeight) {
+      //nav.style.paddingBottom = `${windowHeight-documentHeight}px`;
+    }
+    this.test = `${windowHeight} / ${documentHeight} / ${windowHeight-documentHeight}`;
+  }
 }
