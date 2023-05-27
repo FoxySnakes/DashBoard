07a072e98ea83c5f8a4200d42518fc477edc718f
FoxySnakes <matthox45@gmail.com>
2023-05-27 12:57:01 +0200

    delete useless function

--- src/app/modules/main-layout/navigation-mobile/navigation-mobile.component.ts
+++ src/app/modules/main-layout/navigation-mobile/navigation-mobile.component.ts
@@ -5,24 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navigation-mobile.component.html',
   styleUrls: ['./navigation-mobile.component.scss', '../../../../assets/icon.scss']
 })
-export class NavigationMobileComponent implements OnInit {
+export class NavigationMobileComponent{
 
-  test = ""
-
-  constructor() { }
-
-  ngOnInit(): void {
-    this.adjustBottom();
-    window.addEventListener('resize', this.adjustBottom);
-  }
-
-  adjustBottom() {
-    const nav = document.getElementById('nav-bottom');
-    let windowHeight = window.innerHeight;
-    let documentHeight = document.documentElement.clientHeight;
-    if (nav && windowHeight != documentHeight) {
-      //nav.style.paddingBottom = `${windowHeight-documentHeight}px`;
-    }
-    this.test = `${windowHeight} / ${documentHeight} / ${windowHeight-documentHeight}`;
-  }
 }
